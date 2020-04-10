8278f729f3b6db6ad0d8562798a97ddd205ad348
guillaumepn <guillaumepn@free.fr>
2020-04-10 13:56:44 +0200

    Fix faulty test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,7 +1,10 @@
 describe("react-anchor-navigation test cases", () => {
   it("Should redirect to the correct anchor based on URL's hash", () => {
     cy.visit("./examples/basic.html#part-3");
-    cy.get("[data-cy=anchor_link_part-1] a").should("have.class", "selected");
+    cy.get("[data-cy=anchor_link_part-1] a").should(
+      "not.have.class",
+      "selected"
+    );
     cy.get("[data-cy=anchor_link_part-3] a").should("have.class", "selected");
   });
 
